7fc5a73362a9f3bc86526d50d38c0da2742b9052
Vimal Yadav <vimal.yadav@kreatetechnologies.com>
2023-04-30 07:40:08 +0530

    Add dist file

---
+++ dist/ticket-booking/assets/data.ts
@@ -0,0 +1,150 @@
+/*TESTING JSON COACH DATA*/
+
+const CoachData = [
+  {
+    "rows": [
+      {
+        "rowNumber": 1,
+        "seats": [
+          { "seatNumber": "1A", "available": true },
+          { "seatNumber": "2A", "available": true },
+          { "seatNumber": "3A", "available": true },
+          { "seatNumber": "4A", "available": true },
+          { "seatNumber": "5A", "available": true },
+          { "seatNumber": "6A", "available": true },
+          { "seatNumber": "7A", "available": true }
+        ]
+      },
+      {
+        "rowNumber": 2,
+        "seats": [
+          { "seatNumber": "1B", "available": true },
+          { "seatNumber": "2B", "available": true },
+          { "seatNumber": "3B", "available": true },
+          { "seatNumber": "4B", "available": true },
+          { "seatNumber": "5B", "available": true },
+          { "seatNumber": "6B", "available": true },
+          { "seatNumber": "7B", "available": true }
+        ]
+      },
+      {
+        "rowNumber": 3,
+        "seats": [
+          { "seatNumber": "1C", "available": false },
+          { "seatNumber": "2C", "available": false },
+          { "seatNumber": "3C", "available": false },
+          { "seatNumber": "4C", "available": false },
+          { "seatNumber": "5C", "available": true },
+          { "seatNumber": "6C", "available": true },
+          { "seatNumber": "7C", "available": true }
+        ]
+      },
+      {
+        "rowNumber": 4,
+        "seats": [
+          { "seatNumber": "1D", "available": true },
+          { "seatNumber": "2D", "available": true },
+          { "seatNumber": "3D", "available": true },
+          { "seatNumber": "4D", "available": true },
+          { "seatNumber": "5D", "available": true },
+          { "seatNumber": "6D", "available": true },
+          { "seatNumber": "7D", "available": true }
+        ]
+      },
+      {
+        "rowNumber": 5,
+        "seats": [
+          { "seatNumber": "1E", "available": false },
+          { "seatNumber": "2E", "available": false },
+          { "seatNumber": "3E", "available": false },
+          { "seatNumber": "4E", "available": false },
+          { "seatNumber": "5E", "available": false },
+          { "seatNumber": "6E", "available": false },
+          { "seatNumber": "7E", "available": false }
+        ]
+      },
+      {
+        "rowNumber": 6,
+        "seats": [
+          { "seatNumber": "1F", "available": true },
+          { "seatNumber": "2F", "available": true },
+          { "seatNumber": "3F", "available": true },
+          { "seatNumber": "4F", "available": true },
+          { "seatNumber": "5F", "available": true },
+          { "seatNumber": "6F", "available": true },
+          { "seatNumber": "7F", "available": true }
+        ]
+      },
+      {
+        "rowNumber": 7,
+        "seats": [
+          { "seatNumber": "1G", "available": true },
+          { "seatNumber": "2G", "available": true },
+          { "seatNumber": "3G", "available": true },
+          { "seatNumber": "4G", "available": true },
+          { "seatNumber": "5G", "available": true },
+          { "seatNumber": "6G", "available": true },
+          { "seatNumber": "7G", "available": true }
+        ]
+      },
+      {
+        "rowNumber": 8,
+        "seats": [
+          { "seatNumber": "1H", "available": true },
+          { "seatNumber": "2H", "available": true },
+          { "seatNumber": "3H", "available": true },
+          { "seatNumber": "4H", "available": true },
+          { "seatNumber": "5H", "available": true },
+          { "seatNumber": "6H", "available": true },
+          { "seatNumber": "7H", "available": true }
+        ]
+      },
+      {
+        "rowNumber": 9,
+        "seats": [
+          { "seatNumber": "1I", "available": true },
+          { "seatNumber": "2I", "available": true },
+          { "seatNumber": "3I", "available": true },
+          { "seatNumber": "4I", "available": true },
+          { "seatNumber": "5I", "available": true },
+          { "seatNumber": "6I", "available": true },
+          { "seatNumber": "7I", "available": true }
+        ]
+      },
+      {
+        "rowNumber": 10,
+        "seats": [
+          { "seatNumber": "1J", "available": true },
+          { "seatNumber": "2J", "available": true },
+          { "seatNumber": "3J", "available": true },
+          { "seatNumber": "4J", "available": true },
+          { "seatNumber": "5J", "available": true },
+          { "seatNumber": "6J", "available": true },
+          { "seatNumber": "7J", "available": true }
+        ]
+      },
+      {
+        "rowNumber": 11,
+        "seats": [
+          { "seatNumber": "1K", "available": true },
+          { "seatNumber": "2K", "available": true },
+          { "seatNumber": "3K", "available": true },
+          { "seatNumber": "4K", "available": true },
+          { "seatNumber": "5K", "available": true },
+          { "seatNumber": "6K", "available": true },
+          { "seatNumber": "7K", "available": true }
+        ]
+      },
+      {
+        "rowNumber": 12,
+        "seats": [
+          { "seatNumber": "1L", "available": true },
+          { "seatNumber": "2L", "available": true },
+          { "seatNumber": "3L", "available": true }
+        ]
+      }
+    ]
+  }
+];
+
+export { CoachData };
